80f1963ade659d9eed2bbca8429f15b60845d99c
bill-moj <167539377+bill-moj@users.noreply.github.com>
2025-01-14 12:38:29 +0000

    Upgrade to v2.0.0 frontend components (#23)
    * Upgrade to v2.0.0 frontend components
    * remove unused njk file

--- server/middleware/setUpAuthentication.ts
+++ server/middleware/setUpAuthentication.ts
@@ -2,6 +2,7 @@ import passport from 'passport'
 import flash from 'connect-flash'
 import { Router } from 'express'
 import { Strategy } from 'passport-oauth2'
+import dpsComponents from '@ministryofjustice/hmpps-connect-dps-components'
 import config from '../config'
 import tokenVerifier from '../data/tokenVerification'
 import { HmppsUser } from '../interfaces/hmppsUser'
@@ -41,10 +42,14 @@ export default function setupAuthentication() {
   router.use(passport.session())
   router.use(flash())
 
-  router.get('/autherror', (_req, res) => {
-    res.status(401)
-    return res.render('autherror')
-  })
+  router.get(
+    '/autherror',
+    dpsComponents.getPageComponents({ dpsUrl: config.serviceUrls.digitalPrison }),
+    (_req, res) => {
+      res.status(401)
+      return res.render('autherror')
+    },
+  )
 
   router.get('/sign-in', passport.authenticate('oauth2'))
 
