f713794d5a5927e2fc042bd9a281ab4407f6f0b4
Sameer Jahangir <sameer.jahangir@justice.gov.uk>
2025-03-12 17:25:27 +0000

    Create data grid in view

--- server/routes/profile-summary/controller.ts
+++ server/routes/profile-summary/controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import KeyworkerApiService from '../../services/keyworkerApi/keyworkerApiService'
+import { components } from '../../@types/keyWorker'
 
 export class ProfileSummaryController {
   constructor(private readonly keyworkerApiService: KeyworkerApiService) {}
@@ -9,9 +10,27 @@ export class ProfileSummaryController {
     const userId = res.locals.user.userId!
 
     const keyworkerData = await this.keyworkerApiService.getKeyworkerDetails(req, prisonId, userId)
+    const keyworkerName = `${keyworkerData.keyworker.firstName} ${keyworkerData.keyworker.lastName}`
+    const keyworkerStatus = keyworkerData.status.description
+    const allocations = this.mapAllocations(keyworkerData.allocations)
 
     res.render('profile-summary/view', {
+      keyworkerName,
+      keyworkerStatus,
       keyworkerData,
+      records: allocations,
+    })
+  }
+
+  private mapAllocations(allocations: components['schemas']['Allocation'][]) {
+    return allocations.map(allocation => {
+      return {
+        prisonerId: allocation.prisoner.prisonNumber,
+        prisonerName: `${allocation.prisoner.firstName} ${allocation.prisoner.lastName}`,
+        location: allocation.location,
+        releaseDate: allocation.releaseDate ? allocation.releaseDate : '-',
+        recentSession: allocation.latestSession?.occurredAt ? allocation.latestSession.occurredAt : '-',
+      }
     })
   }
 }
