71c2a7d881c2e8bcc3418e7b4334a01bcd44edfc
Sameer Jahangir <sameer.jahangir@justice.gov.uk>
2025-03-13 13:44:15 +0000

    Add stats data

--- server/routes/profile-summary/controller.ts
+++ server/routes/profile-summary/controller.ts
@@ -13,10 +13,12 @@ export class ProfileSummaryController {
     const keyworkerName = `${keyworkerData.keyworker.firstName} ${keyworkerData.keyworker.lastName}`
     const keyworkerStatus = keyworkerData.status.description
     const allocationRecords = this.mapAllocationsToRecords(keyworkerData.allocations)
+    const keyworkerStats = this.formatStats(keyworkerData.stats.current, keyworkerData.stats.previous)
 
     res.render('profile-summary/view', {
       keyworkerName,
       keyworkerStatus,
+      stats: keyworkerStats,
       keyworkerData,
       records: allocationRecords,
     })
@@ -35,4 +37,68 @@ export class ProfileSummaryController {
       }
     })
   }
+
+  private formatStats = (
+    current: components['schemas']['KeyworkerStats']['current'],
+    previous: components['schemas']['KeyworkerStats']['previous'],
+  ) => {
+    if (!current) return []
+
+    const items = {
+      projectedSessions: { heading: 'Projected sessions', type: 'number' },
+      recordedSessions: {
+        heading: 'Recorded sessions',
+        type: 'number',
+      },
+      complianceRate: {
+        heading: 'Session compliance',
+        type: 'percentage',
+      },
+      recordedEntries: {
+        heading: 'Case notes written',
+        type: 'number',
+      },
+    }
+
+    return Object.entries(items).map(([key, val]) => {
+      const currentVal = current[key as keyof components['schemas']['KeyworkerStats']['current']] as number
+      const previousVal = previous
+        ? (previous[key as keyof components['schemas']['KeyworkerStats']['previous']] as number)
+        : undefined
+
+      return {
+        name: key,
+        heading: val.heading,
+        value: formatValue(currentVal, val.type || 'number'),
+        change: {
+          value: formatChange(
+            previousVal ? formatNumber(currentVal - previousVal, val.type) : currentVal || 0,
+            val.type,
+          ),
+          period: 'period',
+        },
+        type: val.type || 'number',
+      }
+    })
+  }
+}
+
+function formatNumber(value: string | number, type?: string) {
+  if (type === 'percentage') return Number(parseFloat(value.toString()).toFixed(2))
+  return Number(value)
+}
+
+function formatValue(value: number, type: string) {
+  if (type === 'percentage') return `${value} %`
+  if (value === 0) return 0
+  if (!value) return '-'
+  return Number(value)
+}
+
+function formatChange(change: number, type: string) {
+  const formatted = formatValue(change, type)
+  if (change > 0) return `+${formatted} increase`
+  if (change < 0) return `${formatted} decrease`
+  if (change === 0) return 'No change'
+  return formatted
 }
