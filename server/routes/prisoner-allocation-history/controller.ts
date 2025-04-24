1d2e28ff84b38d6bd9a6aeb34eed214657e48e7f
Sameer Jahangir <sameer.jahangir@justice.gov.uk>
2025-04-24 12:50:08 +0100

    Initial screen design

--- server/routes/prisoner-allocation-history/controller.ts
+++ server/routes/prisoner-allocation-history/controller.ts
@@ -5,6 +5,11 @@ export class PrisonerAllocationHistoryController {
   constructor(private readonly prisonerApiService: PrisonApiService) {}
 
   GET = async (req: Request, res: Response, prisonerId: string): Promise<void> => {
-    const prisonerImage = this.prisonerApiService.getPrisonerImage(req, prisonerId)
+    // const prisonerImage = this.prisonerApiService.getPrisonerImage(req, prisonerId)
+
+    const allocationHistoryRecords : string[] = []
+    res.render('prisoner-allocation-history/view',
+      allocationHistoryRecords
+    )
   }
 }
