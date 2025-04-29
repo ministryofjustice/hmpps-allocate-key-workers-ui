4e1d2fd0773ba38388730710d2515b4aa31d077b
Sameer Jahangir <sameer.jahangir@justice.gov.uk>
2025-04-29 08:14:41 +0100

    Add breadcrumbs

--- server/routes/prisoner-allocation-history/controller.ts
+++ server/routes/prisoner-allocation-history/controller.ts
@@ -13,6 +13,7 @@ export class PrisonerAllocationHistoryController {
     const keyworkerAllocations = await this.keyworkerApiService.getKeyworkerAllocations(req, prisonerId)
 
     res.render('prisoner-allocation-history/view', {
+      showBreadcrumbs: true,
       prisoner,
       allocationHistory: keyworkerAllocations.allocations,
     })
