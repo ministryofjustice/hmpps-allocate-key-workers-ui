a3f2dcde8b204d367daebbfa31f75ad0981b600e
bill-moj <bill.yeung@digital.justice.gov.uk>
2025-06-11 15:31:58 +0100

    Update establishment settings page

--- server/routes/establishment-settings/tests.cy.ts
+++ server/routes/establishment-settings/tests.cy.ts
@@ -1,18 +1,18 @@
 import AuthorisedRoles from '../../authentication/authorisedRoles'
 
 context('/establishment-settings', () => {
-  const getCapacityInput = () =>
-    cy.findByRole('textbox', { name: 'Maximum number of prisoners to be allocated to each key worker' })
+  const getCapacityInput = (policyName: string = 'key worker') =>
+    cy.findByRole('textbox', { name: `Maximum number of prisoners to be allocated to each ${policyName}` })
 
   beforeEach(() => {
     cy.task('reset')
     cy.task('stubComponents')
-    cy.task('stubEnabledPrison')
     cy.task('stubKeyworkerApiStatusIsKeyworker')
     cy.task('stubPutPrisonConfiguration')
   })
 
   it('should test admin view', () => {
+    cy.task('stubEnabledPrison')
     cy.task('stubSignIn', {
       roles: [AuthorisedRoles.KW_MIGRATION],
     })
@@ -53,6 +53,7 @@ context('/establishment-settings', () => {
   })
 
   it('should test non-admin view', () => {
+    cy.task('stubEnabledPrison')
     cy.task('stubSignIn', {
       roles: [AuthorisedRoles.OMIC_ADMIN],
     })
@@ -89,6 +90,61 @@ context('/establishment-settings', () => {
     )
   })
 
+  it('should allow admin to enable service in a prison', () => {
+    cy.task('stubPrisonNotEnabled')
+    cy.task('stubSignIn', {
+      roles: [AuthorisedRoles.KW_MIGRATION],
+    })
+
+    navigateToTestPage('personal-officer')
+    cy.url().should('match', /\/establishment-settings$/)
+
+    cy.findByRole('heading', { name: 'Establishment settings for Leeds (HMP)' }).should('be.visible')
+    cy.findByRole('radio', { name: 'Yes' }).should('exist').and('be.checked')
+    getCapacityInput('personal officer').should('be.visible').and('have.value', '9')
+    cy.findByRole('button', { name: 'Save' }).should('be.visible')
+    cy.findByRole('button', { name: 'Cancel' })
+      .should('be.visible')
+      .and('have.attr', 'href')
+      .should('equal', '/personal-officer')
+
+    cy.findByRole('combobox', { name: 'How often should personal officer sessions take place?' })
+      .should('be.visible')
+      .and('have.value', '1WK')
+
+    getCapacityInput('personal officer').clear().type('-1')
+    cy.findByRole('button', { name: 'Save' }).click()
+
+    cy.findByRole('link', { name: /Enter a number between 1 and 999$/i })
+      .should('be.visible')
+      .click()
+    getCapacityInput('personal officer').should('be.focused')
+
+    cy.findByRole('radio', { name: 'No' }).click()
+    getCapacityInput('personal officer').clear().type('12')
+    cy.findByRole('combobox', { name: 'How often should personal officer sessions take place?' }).select(
+      'Every 3 weeks',
+    )
+
+    cy.findByRole('button', { name: 'Save' }).click()
+
+    cy.get('.govuk-notification-banner__heading')
+      .should('be.visible')
+      .and('contain.text', 'Establishment settings updated')
+
+    cy.verifyLastAPICall(
+      { method: 'PUT', urlPath: '/keyworker-api/prisons/LEI/configurations' },
+      {
+        isEnabled: true,
+        hasPrisonersWithHighComplexityNeeds: false,
+        allowAutoAllocation: false,
+        maximumCapacity: 12,
+        capacity: 12,
+        frequencyInWeeks: 3,
+      },
+    )
+  })
+
   const verifyPageCommonContent = () => {
     cy.findByRole('heading', { name: 'Establishment settings for Leeds (HMP)' }).should('be.visible')
     cy.findByRole('radio', { name: 'Yes' }).should('exist').and('be.checked')
@@ -110,8 +166,8 @@ context('/establishment-settings', () => {
     getCapacityInput().should('be.focused')
   }
 
-  const navigateToTestPage = () => {
+  const navigateToTestPage = (policy: string = 'key-worker') => {
     cy.signIn({ failOnStatusCode: false })
-    cy.visit('/key-worker/establishment-settings', { failOnStatusCode: false })
+    cy.visit(`/${policy}/establishment-settings`, { failOnStatusCode: false })
   }
 })
