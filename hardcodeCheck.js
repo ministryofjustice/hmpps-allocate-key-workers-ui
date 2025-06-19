339c6987f681bb42266413f7a5b50266b765e31d
Jack Reeve <jack.reeve@digital.justice.gov.uk>
2025-06-19 16:55:57 +0100

    PR comments

--- hardcodeCheck.js
+++ hardcodeCheck.js
@@ -4,23 +4,25 @@ const { execSync } = require('child_process')
 const fs = require('fs')
 const { sep } = require('path')
 
-const HARDCODED_POLICIES = /(key worker|personal officer)/i
-const fileWhitelist = [/\.ts$/i, /\.njk$/i]
-const fileBlacklist = [/integration_tests/i, /\.cy\.ts$/i, /\.d\.ts$/i]
+const FORBIDDEN_HARDCODES = /(key worker|personal officer)/i
+const FILE_INCLUDES = [/\.ts$/i, /\.njk$/i]
+const FILE_EXCLUDES = [/integration_tests/i, /\.cy\.ts$/i, /\.d\.ts$/i]
 
-const IGNORE_PATTERNS = fs
-  .readFileSync(`${__dirname}${sep}.policyawareignore`, 'utf-8')
-  .split('\n')
-  .slice(1)
-  .filter(Boolean)
-  .map(line => new RegExp(line))
+const IGNORE_PATTERNS = fs.existsSync(`${__dirname}${sep}.hardcodecheckignore`)
+  ? fs
+      .readFileSync(`${__dirname}${sep}.hardcodecheckignore`, 'utf-8')
+      .split('\n')
+      .slice(1)
+      .filter(Boolean)
+      .map(line => new RegExp(line))
+  : []
 
 try {
   const output = execSync('git ls-files', { encoding: 'utf-8' })
   const files = output
     .split('\n')
     .map(f => f.trim())
-    .filter(f => fileWhitelist.some(re => re.test(f)) && !fileBlacklist.some(re => re.test(f)))
+    .filter(f => FILE_INCLUDES.some(re => re.test(f)) && !FILE_EXCLUDES.some(re => re.test(f)))
 
   const matches = []
 
@@ -28,7 +30,7 @@ try {
     const content = fs.readFileSync(file, 'utf-8')
     const lines = content.split('\n')
     lines.forEach((line, i) => {
-      if (HARDCODED_POLICIES.test(line) && !shouldIgnore(file, line)) {
+      if (FORBIDDEN_HARDCODES.test(line) && !shouldIgnore(file, line)) {
         matches.push(`${file}:${i + 1}: ${line.trim()}`)
       }
     })
