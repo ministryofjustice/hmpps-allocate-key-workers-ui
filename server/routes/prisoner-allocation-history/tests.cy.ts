01257816531915582f81401488901ffaba31e0f6
Jack Reeve <jack.reeve@digital.justice.gov.uk>
2025-06-20 15:26:43 +0100

    PR comments

--- server/routes/prisoner-allocation-history/tests.cy.ts
+++ server/routes/prisoner-allocation-history/tests.cy.ts
@@ -40,7 +40,7 @@ context('Prisoner Allocation History', () => {
     cy.get('h1').should('have.text', 'Cat, Tabby (A9965EA)')
 
     cy.get('.govuk-heading-l').eq(0).should('have.text', 'Prisoner key worker allocation history')
-    cy.get('.govuk-heading-m').eq(0).should('have.text', 'Current and previous allocations: 2')
+    cy.findByText('Current and previous allocations: 2').should('be.visible')
 
     // Current key worker card
     cy.get('.govuk-summary-card__title')
@@ -50,169 +50,29 @@ context('Prisoner Allocation History', () => {
         expect(text.trim()).to.equal('Current key worker: Sample Keyworker')
       })
 
-    cy.get('.govuk-summary-list__key')
-      .eq(0)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Establishment')
-      })
-    cy.get('.govuk-summary-list__value')
-      .eq(0)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Moorland (HMP & YOI)')
-      })
+    cy.contains('dt', 'Establishment').next().should('include.text', 'Moorland (HMP & YOI)')
 
-    cy.get('.govuk-summary-list__key')
-      .eq(1)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Allocated on')
-      })
-    cy.get('.govuk-summary-list__value')
-      .eq(1)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('17/04/2025 14:41')
-      })
-
-    cy.get('.govuk-summary-list__key')
-      .eq(2)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Allocated by')
-      })
-    cy.get('.govuk-summary-list__value')
-      .eq(2)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Test Keyworker')
-      })
-
-    cy.get('.govuk-summary-list__key')
-      .eq(3)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Deallocated on')
-      })
-    cy.get('.govuk-summary-list__value')
-      .eq(3)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('-')
-      })
-
-    cy.get('.govuk-summary-list__key')
-      .eq(4)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Deallocated by')
-      })
-    cy.get('.govuk-summary-list__value')
-      .eq(4)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('-')
-      })
-
-    cy.get('.govuk-summary-list__key')
-      .eq(5)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Deallocation reason')
-      })
-    cy.get('.govuk-summary-list__value')
-      .eq(5)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('-')
-      })
+    cy.contains('dt', 'Allocated on').next().should('include.text', '17/04/2025 14:41')
+    cy.contains('dt', 'Allocated by').next().should('include.text', 'Test Keyworker')
+    cy.contains('dt', 'Deallocated on').next().should('include.text', '-')
+    cy.contains('dt', 'Deallocated by').next().should('include.text', '-')
+    cy.contains('dt', 'Deallocation reason').next().should('include.text', '-')
 
     // Previous key worker card
-    cy.get('.govuk-summary-card__title')
-      .eq(1)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Previous key worker: Smith Last-Name')
-      })
+    cy.get('.govuk-summary-card__title').eq(1).should('include.text', 'Previous key worker: Smith Last-Name')
+    cy.get('.govuk-summary-list__key').eq(6).should('include.text', 'Establishment')
+    cy.get('.govuk-summary-list__value').eq(6).should('include.text', 'Moorland (HMP & YOI)')
 
-    cy.get('.govuk-summary-list__key')
-      .eq(6)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Establishment')
-      })
-    cy.get('.govuk-summary-list__value')
-      .eq(6)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Moorland (HMP & YOI)')
-      })
-
-    cy.get('.govuk-summary-list__key')
-      .eq(7)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Allocated on')
-      })
-    cy.get('.govuk-summary-list__value')
-      .eq(7)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('18/12/2024 10:56')
-      })
-
-    cy.get('.govuk-summary-list__key')
-      .eq(8)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Allocated by')
-      })
-    cy.get('.govuk-summary-list__value')
-      .eq(8)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Foo Baz')
-      })
-
-    cy.get('.govuk-summary-list__key')
-      .eq(9)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Deallocated on')
-      })
-    cy.get('.govuk-summary-list__value')
-      .eq(9)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('12/02/2025 15:57')
-      })
-
-    cy.get('.govuk-summary-list__key')
-      .eq(10)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Deallocated by')
-      })
-    cy.get('.govuk-summary-list__value')
-      .eq(10)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Fake Doe')
-      })
-
-    cy.get('.govuk-summary-list__key')
-      .eq(11)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Deallocation reason')
-      })
-    cy.get('.govuk-summary-list__value')
-      .eq(11)
-      .invoke('text')
-      .then(text => {
-        expect(text.trim()).to.equal('Manual')
-      })
+    cy.get('.govuk-summary-list__key').eq(7).should('include.text', 'Allocated on')
+    cy.get('.govuk-summary-list__value').eq(7).should('include.text', '18/12/2024 10:56')
+    cy.get('.govuk-summary-list__key').eq(8).should('include.text', 'Allocated by')
+    cy.get('.govuk-summary-list__value').eq(8).should('include.text', 'Foo Baz')
+    cy.get('.govuk-summary-list__key').eq(9).should('include.text', 'Deallocated on')
+    cy.get('.govuk-summary-list__value').eq(9).should('include.text', '12/02/2025 15:57')
+    cy.get('.govuk-summary-list__key').eq(10).should('include.text', 'Deallocated by')
+    cy.get('.govuk-summary-list__value').eq(10).should('include.text', 'Fake Doe')
+    cy.get('.govuk-summary-list__key').eq(11).should('include.text', 'Deallocation reason')
+    cy.get('.govuk-summary-list__value').eq(11).should('include.text', 'Manual')
   })
 
   const navigateToTestPage = () => {
