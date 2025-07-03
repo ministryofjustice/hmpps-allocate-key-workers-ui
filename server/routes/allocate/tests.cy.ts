ed840280f9ca68a3044234301ecb3f211766c9c5
bill-moj <167539377+bill-moj@users.noreply.github.com>
2025-07-03 16:05:07 +0100

    Fix staff name format on /allocate page (#166)

--- server/routes/allocate/tests.cy.ts
+++ server/routes/allocate/tests.cy.ts
@@ -377,8 +377,8 @@ context('/allocate', () => {
 
     cy.get('.govuk-table__row').eq(3).children().eq(0).should('contain.text', 'John, Doe')
     cy.get('.govuk-table__row').eq(3).children().eq(1).should('contain.text', '1-1-035')
-    cy.get('.govuk-table__row').eq(3).children().eq(3).should('contain.text', 'Available-Active Key-Worker')
-    cy.findByRole('link', { name: 'Available-Active Key-Worker' }).should('have.attr', 'href', 'staff-profile/488095')
+    cy.get('.govuk-table__row').eq(3).children().eq(3).should('contain.text', 'Key-Worker, Available-Active')
+    cy.findByRole('link', { name: 'Key-Worker, Available-Active' }).should('have.attr', 'href', 'staff-profile/488095')
 
     cy.get('.govuk-table__row')
       .eq(3)
