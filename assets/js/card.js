81791227b0f51facf7d2e2e995755cd3e2bf6232
bill-moj <bill.yeung@digital.justice.gov.uk>
2025-09-10 16:19:37 +0100

    Address PR comments

--- assets/js/card.js
+++ assets/js/card.js
@@ -4,6 +4,10 @@ function Card(container) {
   const link = this.container.querySelector('a')
   if (link !== null) {
     link.addEventListener('click', e => {
+      if (e.target.classList.contains('disable-link')) {
+        e.preventDefault()
+      }
+
       e.stopPropagation()
     })
     this.container.addEventListener('click', e => {
