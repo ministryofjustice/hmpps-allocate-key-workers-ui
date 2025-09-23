9dc414750f8a045adea145edc6b53cf631aa6d69
Jack Reeve <jack.reeve@digital.justice.gov.uk>
2025-09-23 11:42:25 +0100

    add test for access with no permissions

--- server/routes/accessibility-statement/tests.cy.ts
+++ server/routes/accessibility-statement/tests.cy.ts
@@ -5,6 +5,15 @@ context('test /accessibility-statement', () => {
     cy.task('stubComponents')
   })
 
+  describe('Role based access', () => {
+    it(`should allow access to users with no permission level`, () => {
+      cy.task('stubSignIn', { roles: [], hasAllocationJobResponsibilities: false })
+      cy.signIn({ failOnStatusCode: false })
+      cy.visit(`accessibility-statement`, { failOnStatusCode: false })
+      cy.url().should('to.match', /\/accessibility-statement$/)
+    })
+  })
+
   it('tests page', () => {
     cy.signIn()
     cy.visit(`accessibility-statement`)
